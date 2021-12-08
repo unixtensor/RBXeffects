b38bb94bd02c6a019228b228361c8ee30e0f2ffa
interpreterK <80614657+interpreterK@users.noreply.github.com>
2021-12-08 12:41:27 -0500

    v0.2 (Have to fix :SpewIn)

--- EffectsModule/src/EffectsModule.lua
+++ EffectsModule/src/EffectsModule.lua
@@ -1,9 +1,7 @@
 -- interpreterK
 -- github.
 
-local Module = {
-	Autoplay = false
-}
+local Module = {Autoplay = false}
 Module.__index = Module
 
 local F = {
@@ -11,6 +9,7 @@ local F = {
 	CN = CFrame.new,
 	ANG = CFrame.Angles,
 	V3 = Vector3.new,
+	RN = Random.new,
 	rad = math.rad,
 	abs = math.abs,
 	sin = math.sin,
@@ -19,6 +18,10 @@ local F = {
 	Euler = CFrame.fromEulerAnglesXYZ
 }
 local TS = game:GetService("TweenService")
+
+local function RandDec(min, max)
+	return F.RN():NextNumber(min, max)
+end
 
 local function EnumExist(EnumType, Name)
 	local b, Enum = pcall(function()
@@ -39,11 +42,11 @@ local function IsAPart(Inst)
 	return typeof(Inst) == "Instance" and Inst:IsA("BasePart")
 end
 
----------
 function Module:ScatterOut(Part, rLimit, vLimit, ...)
 	if IsAPart(Part) then
 		rLimit = rLimit or {-360, 360}
 		vLimit = vLimit or {-15, 15}
+
 		local Tween = TS:Create(Part, TweeningInfo(...), {
 			CFrame = Part.CFrame * (F.ANG(F.rad(F.rand(unpack(rLimit))),F.rad(F.rand(unpack(rLimit))),F.rad(F.rand(unpack(rLimit))))) * (F.CN(F.rand(unpack(vLimit)),F.rand(unpack(vLimit)),F.rand(unpack(vLimit))))
 		})
@@ -57,6 +60,7 @@ end
 function Module:SizeOut(Part, sLimit, ...)
 	if IsAPart(Part) then
 		sLimit = sLimit or {0, 0, 0}
+
 		local Tween = TS:Create(Part, TweeningInfo(...), {
 			Size = F.V3(unpack(sLimit))
 		})
@@ -70,8 +74,9 @@ end
 function Module:FadeOut(Part, Stop, ...)
 	if IsAPart(Part) then
 		Stop = Stop and Stop < 1 and Stop or 1
+
 		local Tween = TS:Create(Part, TweeningInfo(...), {
-			LocalTransparencyModifier = Stop
+			Transparency = Stop
 		})
 		if self.Autoplay then
 			Tween:Play()
@@ -83,8 +88,9 @@ end
 function Module:FadeIn(Part, Stop, ...)
 	if IsAPart(Part) then
 		Stop = Stop and Stop > 1 and Stop or 0
+
 		local Tween = TS:Create(Part, TweeningInfo(...), {
-			LocalTransparencyModifier = Stop
+			Transparency = Stop
 		})
 		if self.Autoplay then
 			Tween:Play()
@@ -97,6 +103,7 @@ function Module:FloatUp(Part, DownBarrier, UpLimit, ...)
 	if IsAPart(Part) then
 		UpLimit = UpLimit or 50
 		DownBarrier = DownBarrier or 30
+
 		Part.CFrame = Part.CFrame - F.V3(0, DownBarrier, 0)
 		local Tween = TS:Create(Part, TweeningInfo(...), {
 			CFrame = Part.CFrame + F.V3(0, UpLimit, 0)
@@ -108,14 +115,15 @@ function Module:FloatUp(Part, DownBarrier, UpLimit, ...)
 	end
 end
 
-function Module:LinearCircle(N, Noffset, D, doffset)
-	return F.CN(0, Noffset * F.sin(N / tick()), 0) * F.CN(0, 0, doffset * F.cos(D / tick()))
-end
-
-function Module:SpinRight(Part, ...)
+function Module:SpewOut(Part, Xlimit, Ylimit, Zlimit, Rlimit, ...)
 	if IsAPart(Part) then
+		Xlimit = Xlimit or {-100, 100}
+		Ylimit = Ylimit or {-100, 100}
+		Zlimit = Zlimit or {-100, 100}
+		Rlimit = Rlimit or {-360, 360}
+
 		local Tween = TS:Create(Part, TweeningInfo(...), {
-			CFrame = Part.CFrame * F.Euler(0, 9000, 9000)
+			CFrame = Part.CFrame * F.CN(RandDec(unpack(Xlimit)), RandDec(unpack(Ylimit)), RandDec(unpack(Zlimit))) * F.ANG(F.rad(RandDec(unpack(Rlimit))), F.rad(RandDec(unpack(Rlimit))), F.rad(RandDec(unpack(Rlimit))))
 		})
 		if self.Autoplay then
 			Tween:Play()
@@ -123,5 +131,25 @@ function Module:SpinRight(Part, ...)
 		return Tween, Part
 	end
 end
+
+function Module:SpewIn(Part, Xlimit, Ylimit, Zlimit, Rlimit, ...)
+	if IsAPart(Part) then
+		local CF = Part.CFrame
+		Xlimit = Xlimit or {-100, 100}
+		Ylimit = Ylimit or {-100, 100}
+		Zlimit = Zlimit or {-100, 100}
+		Rlimit = Rlimit or {-360, 360}
+
+		Part.CFrame *= F.CN(RandDec(unpack(Xlimit)), RandDec(unpack(Ylimit)), RandDec(unpack(Zlimit))) * F.ANG(F.rad(RandDec(unpack(Rlimit))), F.rad(RandDec(unpack(Rlimit))), F.rad(RandDec(unpack(Rlimit))))
+		local Tween = TS:Create(Part, TweeningInfo(...), {
+			CFrame = CF
+		})
+		if self.Autoplay then
+			Tween:Play()
+		end
+		return Tween, Part
+	end
+end
+
 
 return Module
